f65274d04bdc0bc68dadb65bb4cc79258d89258f
Jerin Lee <taurenjerin@qq.com>
2017-12-13 15:02:57 +0800

    增加百度统计。

---
+++ assets/js/baidu-analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?348fbfc7512be02aed1f94c0ec128929";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
